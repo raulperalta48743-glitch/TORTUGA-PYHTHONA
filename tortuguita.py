3969048ea250213ffa49701d83da9dda5afae95a
raulperalta48743-glitch <raulperalta48743@gmail.com>
2026-05-07 21:17:22 -0600

    Implement turtle graphics for circle drawing
    Added turtle graphics to draw circles in a loop.

--- tortuguita.py
+++ tortuguita.py
@@ -1,1 +1,15 @@
+import turtle
 
+ventana = turtle.Screen()
+ventana.bgcolor("black")
+tortuga = turtle.Turtle()
+tortuga.width(10)
+tortuga.speed(1000)
+tortuga.hideturtle()
+tortuga.pencolor("red")
+circulos = 7500000000000000000
+
+for i in range(circulos):
+    tortuga.circle(i * 2)
+    tortuga.right(10)
+turtle.done()
